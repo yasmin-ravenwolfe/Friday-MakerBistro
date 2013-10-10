913e7787d052a1b5bf78e52af668f247f27da2ac
Alex Levine <a17levine@gmail.com>
2013-10-10 10:59:50 -0500

    initial commit with some models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Makerbistro::Application.config.secret_key_base = 'e37d25445d8f44c218602b462b140c6616babe57d1d4d55d12d265de2e3b8df0b69b0df34fe9d33559ecb75fe5eea8f11b96505aae52f934224743fd7320a50a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Makerbistro::Application.config.session_store :cookie_store, key: '_makerbistro_session'
